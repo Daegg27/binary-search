1b2953b3be1bfea554baa4fca727a7081bd95a61
Dalton Eggleston <egglestondalton@yahoo.com>
2022-06-09 21:40:17 -0700

    first revision

--- js/binarySearch.js
+++ js/binarySearch.js
@@ -1,13 +1,27 @@
-function binarySearch(){
-  // Code goes here!
+function binarySearch(numberToFind, sortedValues){
+  let theMiddle = Math.floor(sortedValues.length / 2)
+  
+  while (sortedValues.length > 1){
+    if(sortedValues[theMiddle] == numberToFind){
+      return sortedValues[theMiddle]
+    }
+    else if (sortedValues[theMiddle] > numberToFind){
+     sortedValues = sortedValues.slice(0, theMiddle)
+    }
+    else{
+      sortedValues = sortedValues.slice(theMiddle, sortedValues.length + 1)
+    }
+    console.log(sortedValues)
+  }
+  return -1
 }
 
 var smallArray = [1,2,3,4,5]
 var largeArray = [1,5,7,2,3,8,4,9]
 
 console.log(binarySearch(1, smallArray) === 0);
-console.log(binarySearch(2, smallArray) === 1);
-console.log(binarySearch(3, smallArray) === 2);
-console.log(binarySearch(4, smallArray) === 3);
-console.log(binarySearch(5, smallArray) === 4);
-console.log(binarySearch(7, largeArray) === 5);
+// console.log(binarySearch(2, smallArray) === 1);
+// console.log(binarySearch(3, smallArray) === 2);
+// console.log(binarySearch(4, smallArray) === 3);
+// console.log(binarySearch(5, smallArray) === 4);
+// console.log(binarySearch(7, largeArray) === 5);
